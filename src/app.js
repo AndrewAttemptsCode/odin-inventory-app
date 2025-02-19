66f8b0ca8da9875e041470d7317876ea7e028314
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-19 15:18:58 +0000

    feat: add routes for the respective paths

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const indexRoute = require('./routes/indexRoute');
+const moviesRouter = require('./routes/moviesRoute');
+const directorsRouter = require('./routes/directorRoute');
+const genreRouter = require('./routes/genreRoute');
 const app = express();
 
 app.use(express.static('public'));
@@ -10,5 +13,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use('/', indexRoute);
+app.use('/movies', moviesRouter);
+app.use('/directors', directorsRouter);
+app.use('/genre', genreRouter);
 
 module.exports = app;
