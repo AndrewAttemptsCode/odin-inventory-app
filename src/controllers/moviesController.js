9a896a1c251bff30328adfdd1ac899f00ab1c093
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-20 14:49:31 +0000

    feat: render movies view with the db query to gather all movies

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,5 +1,8 @@
-const allMoviesGet = (req, res) => {
-  res.send('All movies live here');
+const db = require('../../db/queries');
+
+const allMoviesGet = async (req, res) => {
+  const movies = await db.getAllMovies();
+  res.render('movies', { title: 'All movies', movies });
 }
 
 module.exports = { allMoviesGet };
