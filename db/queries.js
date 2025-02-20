172405f8503ceee64c6794d1e673245ff801d8e4
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-20 14:41:46 +0000

    feat: query to get all unique genres

--- db/queries.js
+++ db/queries.js
@@ -1,1 +1,8 @@
 const pool = require('./pool');
+
+const getAllGenres = async () => {
+  const { rows } = await pool.query('SELECT DISTINCT category FROM genres ORDER BY category ASC;');
+  return rows;
+}
+
+module.exports = { getAllGenres };
