c8aaa412884df682394d62d6597dd9afeaf7815f
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-22 15:38:03 +0000

    refactor: get all directors from db query, add error handling and passing returned db query to directors view

--- src/controllers/directorController.js
+++ src/controllers/directorController.js
@@ -1,5 +1,14 @@
-const allDirectorsGet = (req, res) => {
-  res.send('All directors live here');
-}
+const db = require('../../db/queries');
+const asyncHandler = require('express-async-handler');
+
+const allDirectorsGet = asyncHandler(async (req, res) => {
+  const directors = await db.getAllDirectors();
+  
+  if (directors.length === 0) {
+    res.status(404).send('No directors found');
+  }
+
+  res.render('directors', { title: 'All Directors', directors });
+})
 
 module.exports = { allDirectorsGet };
