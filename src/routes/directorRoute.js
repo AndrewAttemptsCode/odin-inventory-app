fc781bb4f4a2dbe757785ad52919bed1c250131f
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-22 16:23:38 +0000

    feat: add dynamic route for selected director from the directors list

--- src/routes/directorRoute.js
+++ src/routes/directorRoute.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
-const { allDirectorsGet } = require('../controllers/directorController');
+const { allDirectorsGet, directorGet } = require('../controllers/directorController');
 
 const directorsRouter = Router();
 
 directorsRouter.get('/', allDirectorsGet);
+directorsRouter.get('/:director', directorGet);
 
 module.exports = directorsRouter;
