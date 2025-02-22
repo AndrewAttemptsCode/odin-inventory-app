aee56df95a787d6ea2ad001df02ef1fabada752f
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-22 14:31:07 +0000

    feat: add error handling for 404 not found status

--- src/controllers/genresController.js
+++ src/controllers/genresController.js
@@ -1,14 +1,25 @@
 const db = require('../../db/queries');
+const asyncHandler = require('express-async-handler');
 
-const allGenreGet = async (req, res) => {
+const allGenreGet = asyncHandler(async (req, res) => {
   const genres = await db.getAllGenres();
-  res.render('genres', { title: 'All genres', genres });
-}
 
-const genreMoviesGet = async (req, res) => {
+  if (genres.length === 0) {
+    return res.status(404).send('No genres found');
+  }
+
+  res.render('genres', { title: 'All genres', genres });
+})
+
+const genreMoviesGet = asyncHandler(async (req, res) => {
   const genre = req.params.genre;
   const movies = await db.getGenreMovies(genre);
+
+  if (movies.length === 0) {
+    return res.status(404).send(`Cannot find genre: ${genre}`);
+  }
+
   res.render('genre', { title: `${genre} movies`, movies })
-}
+})
 
 module.exports = { allGenreGet, genreMoviesGet };
