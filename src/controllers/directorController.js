d406ed762deda5e7bf71722fcfe4906c6ceaf3be
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-23 17:08:25 +0000

    refactor: add get movies by director with external api for the movie posters

--- src/controllers/directorController.js
+++ src/controllers/directorController.js
@@ -1,6 +1,8 @@
 const db = require('../../db/queries');
 const asyncHandler = require('express-async-handler');
 
+const OMDB_API_KEY = process.env.OMDB_API_KEY;
+
 const allDirectorsGet = asyncHandler(async (req, res) => {
   const directors = await db.getAllDirectors();
   
@@ -14,12 +16,23 @@ const allDirectorsGet = asyncHandler(async (req, res) => {
 const directorGet = asyncHandler(async (req, res) => {
   const directorName = req.params.director;
   const director = await db.getDirector(directorName);
+  const movies = await db.getMoviesByDirector(directorName);
 
   if (!director) {
     return res.status(404).send(`Cannot find director: ${directorName}`);
   }
 
-  res.render('director', { title: 'Director Info', director });
+  await Promise.all(
+    movies.map(async (movie) => {
+      const movieTitle = encodeURIComponent(movie.title);
+      const apiURL = `https://www.omdbapi.com/?t=${movieTitle}&apikey=${OMDB_API_KEY}`;
+      const response = await fetch(apiURL);
+      const data = await response.json();
+      movie.poster = data.Poster;
+    })
+  )
+
+  res.render('director', { title: 'Director Info', director, movies });
 })
 
 module.exports = { allDirectorsGet, directorGet };
