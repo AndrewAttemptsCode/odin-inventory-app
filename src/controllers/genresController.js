d00d97c4d29c6a90d53e8de047776ac7dbf42b26
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-24 11:33:18 +0000

    feat: add external api for movie poster image

--- src/controllers/genresController.js
+++ src/controllers/genresController.js
@@ -1,6 +1,8 @@
 const db = require('../../db/queries');
 const asyncHandler = require('express-async-handler');
 
+const OMDB_API_KEY = process.env.OMDB_API_KEY;
+
 const allGenreGet = asyncHandler(async (req, res) => {
   const genres = await db.getAllGenres();
 
@@ -18,6 +20,16 @@ const genreMoviesGet = asyncHandler(async (req, res) => {
   if (movies.length === 0) {
     return res.status(404).send(`Cannot find genre: ${genre}`);
   }
+
+  await Promise.all(
+    movies.map(async (movie) => {
+      const movieTitle = encodeURIComponent(movie.title);
+      const apiURL = `https://www.omdbapi.com/?t=${movieTitle}&apikey=${OMDB_API_KEY}`;
+      const response = await fetch(apiURL);
+      const data = await response.json();
+      movie.poster = data.Poster;
+    })
+  )
 
   res.render('genre', { title: `${genre} movies`, movies })
 })
