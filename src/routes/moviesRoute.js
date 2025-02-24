d14aa5437dd9f072a3d260ffc8d667947f161bcf
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-24 14:41:32 +0000

    feat: add movie form get and post routes

--- src/routes/moviesRoute.js
+++ src/routes/moviesRoute.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
-const { allMoviesGet, movieGet } = require('../controllers/moviesController');
+const { allMoviesGet, movieGet, movieFormGet, movieFormPost } = require('../controllers/moviesController');
 
 const moviesRouter = Router();
 
 moviesRouter.get('/', allMoviesGet);
+moviesRouter.get('/add-movie', movieFormGet);
+moviesRouter.post('/add-movie', movieFormPost);
 moviesRouter.get('/:movie', movieGet);
 
 module.exports = moviesRouter;
