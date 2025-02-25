7e4f5fc1294538b5fac5b024b43466b9097aaff9
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-02-25 15:29:26 +0000

    feat: add get route for the edit movie form

--- src/routes/moviesRoute.js
+++ src/routes/moviesRoute.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
-const { allMoviesGet, movieGet, movieFormGet, movieFormPost } = require('../controllers/moviesController');
+const { allMoviesGet, movieGet, movieFormGet, movieFormPost, movieEditFormGet } = require('../controllers/moviesController');
 
 const moviesRouter = Router();
 
 moviesRouter.get('/', allMoviesGet);
 moviesRouter.get('/add-movie', movieFormGet);
 moviesRouter.post('/add-movie', movieFormPost);
+moviesRouter.get('/:movie/edit', movieEditFormGet);
 moviesRouter.get('/:movie', movieGet);
 
 module.exports = moviesRouter;
