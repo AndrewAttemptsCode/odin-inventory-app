fc8c6cc111184716aada9f2f309974f7b9193f4b
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-03-01 15:16:31 +0000

    feat: add post route for the add new director form

--- src/routes/directorRoute.js
+++ src/routes/directorRoute.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-const { allDirectorsGet, directorGet, directorFormGet } = require('../controllers/directorController');
+const { allDirectorsGet, directorGet, directorFormGet, directorFormPost } = require('../controllers/directorController');
 
 const directorsRouter = Router();
 
 directorsRouter.get('/', allDirectorsGet);
 directorsRouter.get('/add-director', directorFormGet);
+directorsRouter.post('/add-director', directorFormPost);
 directorsRouter.get('/:director', directorGet);
 
 module.exports = directorsRouter;
