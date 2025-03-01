d1132378468225249fcf46f8655b63974b3e4708
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-03-01 13:58:15 +0000

    feat: add routing to get new director form

--- src/routes/directorRoute.js
+++ src/routes/directorRoute.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-const { allDirectorsGet, directorGet } = require('../controllers/directorController');
+const { allDirectorsGet, directorGet, directorFormGet } = require('../controllers/directorController');
 
 const directorsRouter = Router();
 
 directorsRouter.get('/', allDirectorsGet);
+directorsRouter.get('/add-director', directorFormGet);
 directorsRouter.get('/:director', directorGet);
 
 module.exports = directorsRouter;
