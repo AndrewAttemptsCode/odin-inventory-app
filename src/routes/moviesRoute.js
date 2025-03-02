30ece5f93a99e37716730d9f683efd747a2e1723
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-03-02 14:36:54 +0000

    feat: add genres post route

--- src/routes/moviesRoute.js
+++ src/routes/moviesRoute.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { allMoviesGet, movieGet, movieFormGet, movieFormPost, movieEditFormGet, movieEditDetailsPost, movieEditDirectorPost } = require('../controllers/moviesController');
+const { allMoviesGet, movieGet, movieFormGet, movieFormPost, movieEditFormGet, movieEditDetailsPost, movieEditDirectorPost, movieEditGenresPost } = require('../controllers/moviesController');
 
 const moviesRouter = Router();
 
@@ -8,7 +8,8 @@ moviesRouter.get('/add-movie', movieFormGet);
 moviesRouter.post('/add-movie', movieFormPost);
 moviesRouter.get('/:movie/edit', movieEditFormGet);
 moviesRouter.post('/:movie/edit/details', movieEditDetailsPost);
-moviesRouter.post('/:movie/edit/director', movieEditDirectorPost)
+moviesRouter.post('/:movie/edit/director', movieEditDirectorPost);
+moviesRouter.post('/:movie/edit/genres', movieEditGenresPost);
 moviesRouter.get('/:movie', movieGet);
 
 module.exports = moviesRouter;
