a2d504d2483fa69d56364d0e2d5bdedc5edcfb1e
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-03-03 14:37:58 +0000

    feat: add get and post routes for new movie genre form

--- src/routes/genresRoute.js
+++ src/routes/genresRoute.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
-const { allGenreGet, genreMoviesGet } = require('../controllers/genresController');
+const { allGenreGet, genreMoviesGet, genreFormGet, genreFormPost } = require('../controllers/genresController');
 
 const genreRouter = Router();
 
 genreRouter.get('/', allGenreGet);
+genreRouter.get('/:movie/add-genre', genreFormGet);
+genreRouter.post('/:movie/add-genre', genreFormPost);
 genreRouter.get('/:genre', genreMoviesGet);
 
 module.exports = genreRouter;
