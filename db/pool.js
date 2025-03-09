6ec4ecbb16eaf21e044073fbb08bf19f62f97708
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-03-09 15:17:00 +0000

    refactor: change to target prod db env

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DEV_DATABASE_URL
+  connectionString: process.env.PROD_DATABASE_URL
 });
 
 module.exports = pool;
